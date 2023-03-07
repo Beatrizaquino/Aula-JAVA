e9e8f281e4e16b3165141c1e33475e80d1cee762
Beatriz aquino <beatrizzoldyck07@gmail.com>
2023-03-06 21:06:54 -0400

    aula 1

--- Filme.java
+++ Filme.java
@@ -20,6 +20,14 @@
         this.duracao = duracao;
     }
 
+    public int getAno() {
+        return this.ano;
+    }
+
+    public void setAno( int ano){
+        this.ano = ano;
+    }
+
     public String getSinopse(){
         return this.sinopse;
     }
--- Principal.java
+++ Principal.java
@@ -3,7 +3,24 @@ public class Principal {
     public static void main(String[] args) {
         
         Filme filme1 =  new Filme();  
-        filme1.setTitulacao("Barbie");
+        filme1.setTitulacao("Titulo: Barbie");
         System.out.println(filme1.getTitulacao());
+
+        Filme sinopse1 = new Filme();
+        sinopse1.setSinopse("Sinopse: Este é um filme muito legal para se assistir");
+        System.out.println(sinopse1.getSinopse());
+
+        Filme duracao = new Filme();
+        duracao.setDuracao(120);
+        System.out.println(duracao.getDuracao()); 
+
+        Filme ano = new Filme();
+        ano.setAno(2022);
+        System.out.println(ano.getAno());
+         
     }
+
+    
+
+    
 }

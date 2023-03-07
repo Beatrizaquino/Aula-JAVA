8106b94f01220aa22f0a88aea5f6048fe07f071c
Beatriz aquino <beatrizzoldyck07@gmail.com>
2023-03-06 20:10:25 -0400

    Projeto cinema

---
+++ Filme.java
@@ -0,0 +1,30 @@
+ class Filme {
+    private String titulo;
+    private int duracao;
+    private int ano;
+    private String sinopse;
+
+    public String getTitulacao() {
+        return this.titulo;
+    }
+
+    public void setTitulacao( String titulo) {
+        this.titulo = titulo;
+    }
+
+    public int getDuracao() {
+        return this.duracao;
+    }
+
+    public void setDuracao( int duracao) {
+        this.duracao = duracao;
+    }
+
+    public String getSinopse(){
+        return this.sinopse;
+    }
+
+    public void setSinopse( String sinopse) {
+        this.sinopse = sinopse;
+    }
+ }
---
+++ Principal.java
@@ -0,0 +1,9 @@
+public class Principal {
+ 
+    public static void main(String[] args) {
+        
+        Filme filme1 =  new Filme();  
+        filme1.setTitulacao("Barbie");
+        System.out.println(filme1.getTitulacao());
+    }
+}

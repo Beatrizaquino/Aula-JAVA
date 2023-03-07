4aeb2723749b89adae50ee276b7521244e7569f0
Beatriz aquino <beatrizzoldyck07@gmail.com>
2023-03-06 21:15:34 -0400

    agora ta bonito

--- Principal.java
+++ Principal.java
@@ -3,20 +3,11 @@ public class Principal {
     public static void main(String[] args) {
         
         Filme filme1 =  new Filme();  
-        filme1.setTitulacao("Titulo: Barbie");
-        System.out.println(filme1.getTitulacao());
-
-        Filme sinopse1 = new Filme();
-        sinopse1.setSinopse("Sinopse: Este é um filme muito legal para se assistir");
-        System.out.println(sinopse1.getSinopse());
-
-        Filme duracao = new Filme();
-        duracao.setDuracao(120);
-        System.out.println(duracao.getDuracao()); 
-
-        Filme ano = new Filme();
-        ano.setAno(2022);
-        System.out.println(ano.getAno());
+        filme1.setTitulacao("Titulo: Barbie ");
+        filme1.setSinopse(" Sinopse: Este é um filme muito legal para se assistir");
+        filme1.setDuracao(120);
+        filme1.setAno(2022);
+        System.out.println("O filme" + filme1.getTitulacao() + ". " + filme1.getSinopse() + ". Tem duração de "  + filme1.getDuracao() + " minutos e foi lançado em " + filme1.getAno());
          
     }
 
